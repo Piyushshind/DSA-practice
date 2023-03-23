81649a456648a5e07e7fa862c8a02a8d771d5102
Piyushshind <piyushshinde.199@gmail.com>
2023-03-23 11:29:24 +0530

    is anagram

---
+++ Practice/Anagram.js
@@ -0,0 +1,17 @@
+var str = 'water';
+var sr = 'retwa'
+
+function anna(sa, ta) {
+    let s= sa.split('').sort()
+    let t= ta.split('').sort()
+    if(s.length == t.length){
+        for(let i=0;i<s.length ; i++){
+            if(s[i]==t[i]) return true
+            return false
+        }
+    }
+    return false
+   
+};
+let ans = anna(str,sr)
+console.log(ans)
