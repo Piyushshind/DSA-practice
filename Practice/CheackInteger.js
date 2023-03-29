767094a9b6cefd703d67b9e30115bbc4b4a0c0ca
Piyushshind <piyushshinde.199@gmail.com>
2023-03-29 21:53:35 +0530

    Cheack is integer

---
+++ Practice/CheackInteger.js
@@ -0,0 +1,12 @@
+
+function cheackInteger(i){
+    // ***********  by using Regex  **********
+//    let cheack = /^-?[0-9]+$/ ;
+//    let result =  cheack.test(i)
+//    return result ;
+
+    //  ********* 
+ 
+}
+
+console.log(cheackInteger(8));
