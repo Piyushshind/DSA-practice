0e98a4d2e610b0eadcddffde1b2b9f22926186f0
Piyushshind <piyushshinde.199@gmail.com>
2023-04-01 23:30:42 +0530

    mock question

--- ByRecursiveFunction/FibonasiNumber.js
+++ ByRecursiveFunction/FibonasiNumber.js
@@ -3,7 +3,8 @@ function fib(n){
 if(n==0) return 0;
 if(n==1 || n==2){ return 1}
 else{
-let nums = fib(n-1) + fib(n-2)  
+let nums = fib(n-1) + fib(n-2) 
+return nums 
 }
        
 }
---
+++ prac1.js
@@ -0,0 +1,25 @@
+// The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+
+// By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
+// "123"
+// "132"
+// "213"
+// "231"
+// "312"
+// "321"
+// Given n and k, return the kth permutation sequence.
+//  
+function arra(arr,n,k){
+     let str ='';
+    for(let i=0;i<k;i++){
+         str += arr[i]
+       for(let j=i+1; j<k; j++){
+        str+=(arr[j])
+       }
+    }
+     return str ;
+
+}
+
+console.log(arra([1,2,3,4,5,6],3,3));
