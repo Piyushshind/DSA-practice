83e70b5fb8ef386f9722693cb80acd9a5bb45c9c
Piyushshind <piyushshinde.199@gmail.com>
2023-04-01 23:58:21 +0530

    find no. which is greater than its index

---
+++ NoIsGreaterThanItsIndex.js
@@ -0,0 +1,14 @@
+// find the number which is greater than its index 
+arr = [0,1,2,3,8,5,6,9,7,5,8,23,7,9,18,67,8,90];
+
+   function findsm(ar){
+    let ans =[];
+      let n = ar.length-1;
+      for(let i =0 ; i<n;i++){
+        if(arr[i]>i) ans.push( arr[i])
+        else 0
+        
+      }
+      return ans;
+   }
+  console.log(findsm(arr));
