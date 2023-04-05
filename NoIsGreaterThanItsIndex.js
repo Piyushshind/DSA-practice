86b6009c4a77a8ebfe16718d3e883e69f1e950d1
Piyushshind <piyushshinde.199@gmail.com>
2023-04-06 00:00:27 +0530

    some

--- NoIsGreaterThanItsIndex.js
+++ NoIsGreaterThanItsIndex.js
@@ -4,6 +4,8 @@ arr = [0,1,2,3,8,5,6,9,7,5,8,23,7,9,18,67,8,90];
    function findsm(ar){
     let ans =[];
       let n = ar.length-1;
+
+      
       for(let i =0 ; i<n;i++){
         if(arr[i]>i) ans.push( arr[i])
         else 0
