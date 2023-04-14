4202d07254dec7e58eb7a53d7208a4983df9a725
Piyushshind <piyushshinde.199@gmail.com>
2023-04-14 10:30:55 +0530

    ceize prime

---
+++ Practice/Prime/ceize.js
@@ -0,0 +1,32 @@
+function sieve(n){
+
+	var prime=new Array(n+1)
+
+	for(let i=0;i<n+1;i++){
+		prime[i]=true
+	}
+
+	prime[0]=false
+	prime[1]=false
+
+	for(let i=2;i*i<=n;i++){//O(nloglogn)
+		if(prime[i]==true){
+			for(let j=i*i;j<=n;j+=i){//multiples
+				prime[j]=false
+			}
+		}
+	}
+
+	let arr=[]
+	let c=0
+	for(let i=2;i<=n;i++){
+		if(prime[i]){
+			arr.push(i)
+			c++
+		}
+	}
+
+	console.log(arr);
+}
+
+sieve(500) ;
