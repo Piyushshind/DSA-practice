55d5b1b50715ccad39f10c3f2e69743a727e45dd
Piyushshind <piyushshinde.199@gmail.com>
2023-04-14 23:29:12 +0530

    1s 2s

--- Sort/1s2s.js
+++ Sort/1s2s.js
@@ -1,27 +1,26 @@
-function sort(arr){
-    let i=0
-    let l=0
-    let r=arr.length-1;
-   while(i<=r){
-      if(arr[i]==0){
-          let temp=arr[i]
-          arr[i]=arr[r]
-          arr[r]=temp
-          r--
-      } else if(arr[i]==1){
-          let temp=arr[i]
-          arr[i]=arr[l]
-          arr[l]=temp
-          i++
-          l++
-      }else{
-          i++
-      }
-     
-   }
-  
-    
-    return arr
+function sort(arr) {
+  let i = 0;
+  let l = 0;
+  let r = arr.length - 1;
+
+  while (i <= r) {
+    if (arr[i] == 0) {
+      let temp = arr[i];
+      arr[i] = arr[r];
+      arr[r] = temp;
+      r--;
+    } else if (arr[i] == 1) {
+      let temp = arr[i];
+      arr[i] = arr[l];
+      arr[l] = temp;
+      i++;
+      l++;
+    } else {
+      i++;
+    }
   }
-  let arr=[0,1,0,0,1,1,0,1,0,1]
-  console.log(sort(arr))
+
+  return arr;
+}
+let arr = [0, 1, 0, 0, 1, 1, 0, 1, 0, 1];
+console.log(sort(arr));
