70701b5d038028fde4599717b53e4f2df1192351
Piyushshind <piyushshinde.199@gmail.com>
2023-04-14 23:28:24 +0530

    1s 2 s

---
+++ Sort/1s2s.js
@@ -0,0 +1,27 @@
+function sort(arr){
+    let i=0
+    let l=0
+    let r=arr.length-1;
+   while(i<=r){
+      if(arr[i]==0){
+          let temp=arr[i]
+          arr[i]=arr[r]
+          arr[r]=temp
+          r--
+      } else if(arr[i]==1){
+          let temp=arr[i]
+          arr[i]=arr[l]
+          arr[l]=temp
+          i++
+          l++
+      }else{
+          i++
+      }
+     
+   }
+  
+    
+    return arr
+  }
+  let arr=[0,1,0,0,1,1,0,1,0,1]
+  console.log(sort(arr))
