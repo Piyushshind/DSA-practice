cd30b010e344ded55e78e1283e7c9a4f08189dc7
Piyushshind <piyushshinde.199@gmail.com>
2023-04-14 11:10:04 +0530

    fibonanci

--- ByRecursiveFunction/FibonasiNumber.js
+++ ByRecursiveFunction/FibonasiNumber.js
@@ -1,11 +1,9 @@
-function fib(n){
-  
-if(n==0) return 0;
-if(n==1 || n==2){ return 1}
-else{
-let nums = fib(n-1) + fib(n-2) 
-return nums 
+function fib(n) {
+  if (n == 0) return 0;
+  if (n == 1 || n == 2) {
+    return 1;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
 }
-       
-}
-console.log(fib(9))
+console.log(fib(9));
