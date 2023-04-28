979a89e257450555dcbc820061427a89fd1ae755
Piyushshind <piyushshinde.199@gmail.com>
2023-04-28 16:28:17 +0530

    Longest Substring Without Repeating Characters

--- Practice/LongestSubstringWithoutRepeatingCharacters.js
+++ Practice/LongestSubstringWithoutRepeatingCharacters.js
@@ -1,12 +1,21 @@
 //  Longest Substring Without Repeating Characters ;
 
-let str = 'abfatyhj';
+let str = 'abfatfyhj';
 
 function longSunString(str){
     if (str=='') return 0;
-    let n=str.length , i=0,j=1,mp=new Map();
+    let n=str.length , i=0,j=1,ans=1,mp=new Map();
     mp.set(str[0],1)
     while(j<n){
-        
+        while(mp.has(str[j])){
+            mp.delete(str[i])
+            i++
+        }
+        mp.set(str[j],1)
+        j++
+        ans = Math.max(j-i,ans)
     }
+    return ans
 }
+
+console.log(longSunString(str));
