766380f5b63f68b2eb9608f6a7280221a9a9b506
Piyushshind <piyushshinde.199@gmail.com>
2023-04-28 16:19:58 +0530

    substring

---
+++ Practice/LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,12 @@
+//  Longest Substring Without Repeating Characters ;
+
+let str = 'abfatyhj';
+
+function longSunString(str){
+    if (str=='') return 0;
+    let n=str.length , i=0,j=1,mp=new Map();
+    mp.set(str[0],1)
+    while(j<n){
+        
+    }
+}
