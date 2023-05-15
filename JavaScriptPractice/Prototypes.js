86d4e165d593f73de952bc32a1e5f16fc04398b2
Piyushshind <piyushshinde.199@gmail.com>
2023-05-15 22:33:26 +0530

    prototype

---
+++ JavaScriptPractice/Prototypes.js
@@ -0,0 +1,11 @@
+let arr = [ 'merry' , 45 , true , 'islint'] ;
+
+Array.prototype.convertArrayToObject = function(){
+      let newObj = {};
+      this.forEach(element => {
+         newObj[element] = element
+      })
+      return newObj;
+}
+
+console.log(arr.convertArrayToObject());
