9ee71363120ff68625231791c402f6e3e7943b66
Piyushshind <piyushshinde.199@gmail.com>
2023-11-11 20:34:50 +0530

    merge

--- Merge/merge.js
+++ Merge/merge.js
@@ -1,30 +1,30 @@
-let a=[2,5,8,9]
-let b=[3,4,7]
+let a = [2, 5, 8, 9]
+let b = [3, 4, 7]
 
-var n=a.length,m=b.length
-let c=new Array(m+n)
-let i=0,j=0,k=0
-while(i<n&&j<m){
-    if(a[i]<b[j]){
-        c[k]=a[i]
+var n = a.length, m = b.length
+let c = new Array(m + n)
+let i = 0, j = 0, k = 0
+while (i < n && j < m) {
+    if (a[i] < b[j]) {
+        c[k] = a[i]
         i++
         k++
     }
-    else{
-        c[k]=b[j]
+    else {
+        c[k] = b[j]
         k++
-         j++
+        j++
     }
 }
 
-while(i<n){
-    c[k]=a[i]
+while (i < n) {
+    c[k] = a[i]
     i++
     k++
 }
 
-while(j<m){
-    c[k]=b[j]
+while (j < m) {
+    c[k] = b[j]
     k++
     j++
 }
