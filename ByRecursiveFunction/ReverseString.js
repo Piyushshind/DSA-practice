963ebdb502d4c0d697b8efd72d5a88564ec7bd8b
Piyushshind <piyushshinde.199@gmail.com>
2023-11-11 20:37:24 +0530

    reverse

--- ByRecursiveFunction/ReverseString.js
+++ ByRecursiveFunction/ReverseString.js
@@ -1,9 +1,9 @@
 let str = 'asfdgjhkl';
-//  console.log(str.replace(str[0],str.slice(-1)));
-function rev(str){
-   if(str.length <=1) return str;
-   return rev(str.slice(1)) + str[0]
-  }
+// console.log(str.replace(str[0],str.slice(-1)));
+function rev(str) {
+  if (str.length <= 1) return str;
+  return rev(str.slice(1)) + str[0]
+}
 console.log(rev(str));
 
 
