0e1ccdd793640f4956c9786fccd13b7bc2470fe7
Piyushshind <piyushshinde.199@gmail.com>
2023-11-11 20:34:13 +0530

    max

--- SlidingWindow/findMaxSum.js
+++ SlidingWindow/findMaxSum.js
@@ -6,14 +6,14 @@ function sliding(arr, k) {
   for (let i = 0; i < k; i++) {
     sum += arr[i];
   }
-  
+
   let max = sum;
 
   for (let j = k; j < arr.length; j++) {
     sum += arr[j] - arr[j - k];
     max = Math.max(sum, max);
   }
-   return max;
+  return max;
 }
 
 console.log(sliding(arr, 4));
