36450b71b26a4f01c2bfc16fd2eda92578dd2571
Piyushshind <piyushshinde.199@gmail.com>
2025-07-28 19:59:04 +0530

    stack implementation

---
+++ stack/simple.js
@@ -0,0 +1,27 @@
+class StackP {
+    constructor() {
+        this.bucket = [];
+    }
+    push(elem) {
+        this.bucket.push(elem)
+    }
+
+    pop() {
+        if (this.bucket.length == 0) return "underflow";
+        this.bucket.pop();
+    }
+
+    getPeekElement(){
+        return this.bucket[this.bucket.length-1]
+    }
+    showAllItems(){
+        return this.bucket.join(' _ ');
+    }
+}
+
+const s = new StackP();
+s.push(15);
+s.push(7);
+s.push(455);
+
+console.log(s.showAllItems());
